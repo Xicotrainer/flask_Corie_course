5868ca2a0c3a4e2871d931cf4fcf228d3c04c531
xicote <xicotrainer@gmail.com>
2022-06-25 00:57:01 -0500

    The project begins. Render html functions is ised

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/<name>')
+def home(name):
+    return render_template('index.html', content=name, r=2)
+
+# @app.route('/<name>')
+# def user(name):
+#     return f'Hi {name}!'
+
+# @app.route('/admin/')
+# def admin():
+#     return redirect(url_for('user', name='Admin!'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# https://www.youtube.com/watch?v=xIgPMguqyws&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX&index=2
