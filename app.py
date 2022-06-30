dfb04e09416e078af3561c1a0b21f0aeb3d3df0b
xicote <xicotrainer@gmail.com>
2022-06-29 23:15:15 -0500

    The post method works. It allows to continue the project

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def home(name):
-    return render_template('index.html', content = ['Tom', 'Sam', 'Jef'])
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# @app.route('/<name>')
-# def user(name):
-#     return f'Hi {name}!'
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    return render_template('login.html')
 
-# @app.route('/admin/')
-# def admin():
-#     return redirect(url_for('user', name='Admin!'))
+@app.route('/<usr>')
+def user(usr):
+    return f'<h1>{usr}</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-# https://www.youtube.com/watch?v=xIgPMguqyws&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX&index=2
+# https://www.youtube.com/watch?v=4nzI4RKwb5I&list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX&index=4
